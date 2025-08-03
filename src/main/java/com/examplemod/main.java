ce38241f147e7a8fc33f0622adcec7d728b7a270
unknow <138153020+ff6f8d68@users.noreply.github.com>
2025-08-03 21:56:48 +0200

    Add files via upload

--- src/main/java/com/examplemod/main.java
+++ src/main/java/com/examplemod/main.java
@@ -1,21 +1,31 @@
 package com.examplemod;
 
-import modloader.konstruct.fundation.mod;
-import modloader.konstruct.fundation.ModInfo;
+import modloader.konstruct.foundation.mod;
+import modloader.konstruct.foundation.ModInfo;
+import modloader.konstruct.events.NamedEvent;
+import modloader.konstruct.events.EventBus;
+import modloader.konstruct.events.SubscribeEvent;
+import modloader.konstruct.utils.logger;
 
 public class main extends mod {
 
     public main() {
-        super(new ModInfo(
-                "Example Mod",
-                "1.0.0",
-                "Example Author",
-                "An example mod for konstruct"
-        ));
+        super(new ModInfo("MyMod", "1.0", "Me", "A dynamic event mod"));
+        EventBus.getGlobal().register(this);
+
+        // Fire your init event
+        EventBus.getGlobal().post(new NamedEvent("mod_init"));
     }
 
-    @Override
-    public void mainhook() {
-        System.out.println("Example mod loaded successfully!");
+    @SubscribeEvent
+    public void onAnyEvent(NamedEvent event) {
+        String name = event.getName();
+        switch (name) {
+            case "mod_init" -> logger.info("Mod init detected");
+            case "game_start" -> logger.info("Game started!");
+            default -> {
+                // handle other dynamic events if needed
+            }
+        }
     }
 }
